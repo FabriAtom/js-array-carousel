aea55f8e5666dd139ce5415d3cac90637ae63f45
FabriAtom <fabrizioromano20@gmail.com>
2022-07-26 18:19:26 +0200

    comm/js

---
+++ js/app.js
@@ -0,0 +1,47 @@
+// console.log('slider')
+
+const slides = [
+	'./img/01.jpg',
+	'./img/02.jpg',
+	'./img/03.jpg',
+	'./img/04.jpg',
+	'./img/05.jpg',
+]
+
+const slideWrapperEl = document.querySelector('.slides-wrapper');
+slideWrapperEl.innerHTML = '';
+console.log(slideWrapperEl);
+
+
+// genera il contenuto dello slider
+
+for (let i = 0; i < slides.length; i++) {
+  const src = slides[i];
+  console.log(src);
+
+//creando li
+  const li = document.createElement('li');
+
+//agg. la classe slide al li 
+  li.className = 'slide';
+
+  if (i === 0){
+    li.classList.add('active');
+  }
+}
+
+
+// creazione img
+
+const img = document.createElement('img');
+// assegnazione src valore preso
+img.src = src
+
+// img dentro all'li
+
+li.append(img);
+
+console.log(li);
+
+// inserendo li dentro slides wrapper 
+slideWrapperEl.append(li);
